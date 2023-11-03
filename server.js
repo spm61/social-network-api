9ca0f2225ced1db9934c55daa581e9914e8cd80b
spmoroney <spmoroney@att.net>
2023-11-03 19:17:17 -0400

    update server, add packages, and define the connection

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./config/connection'); //to be defined
+const routes = require('./routes') //to be defined
+
+const PORT = 3001; //since we're not deploying to heroku, we're just using a port number.
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes); 
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
