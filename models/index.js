ed9bf5de7fa80c2d655e0f09bddd4cce56c38aeb
spmoroney <spmoroney@att.net>
2023-11-03 19:49:18 -0400

    Add user model and index file for models.

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require("./user");
+const Thought = require("./thought");
+
+//this file is a sort of entrypoint to the models so it can be referred to by itself.
+module.exports = { User, Thought };
