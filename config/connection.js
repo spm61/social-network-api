52ceef2ce204f3e1fd4620b13cd6d9e460fc1e33
spmoroney <spmoroney@att.net>
2023-11-03 21:42:13 -0400

    need to use local IP instead of localhost.

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/social-network-db', {
+mongoose.connect('mongodb://127.0.0.1:27017/social-network-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
